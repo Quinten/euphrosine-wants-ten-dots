2050454ae1637dfaf5fb1b2e0e5e00fda0f62add
Quinten Clause <quinten.clause@studioemma.eu>
2016-10-16 22:51:02 +0200

    Adds menuState

--- code/game.js
+++ code/game.js
@@ -81,6 +81,64 @@ var loadState = {
 
 };
 
+var menuState = {
+
+    menuGroup: undefined,
+    switched: false,
+
+    create: function () {
+
+        this.menuGroup = game.add.group();
+        this.menuGroup.x = game.world.centerX;
+        this.menuGroup.y = game.world.centerY;
+
+        var textsprite = this.menuGroup.add(this.createText(0, -80, 'Ready?\nHit spacebar'));
+
+        //  Register the key.
+        this.spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
+        //  Stop the following key from propagating up to the browser
+        game.input.keyboard.addKeyCapture([ Phaser.Keyboard.SPACEBAR ]);
+
+        this.switched = false;
+    },
+
+    update: function () {
+
+        if (this.spaceKey.downDuration(1000) && !this.switched) {
+            console.log('switched');
+            this.switched = true;
+            //game.state.start('game');
+        }
+    },
+
+    resize: function () {
+
+        this.menuGroup.x = game.world.centerX;
+        this.menuGroup.y = game.world.centerY;
+
+    },
+
+    shutdown: function () {
+
+        this.menuGroup = undefined;
+
+    },
+
+    createText: function (x, y, text) {
+
+        var textSprite = game.add.text(x, y, text);
+        textSprite.anchor.setTo(0.5);
+        textSprite.font = fontName;
+        textSprite.fontSize = 32;
+        textSprite.fill = colors.normalStroke;
+        textSprite.align = 'center';
+
+        return textSprite;
+
+    }
+};
+
 var splashState = {
 
     text: undefined,
@@ -98,7 +156,7 @@ var splashState = {
         text.align = 'center';
 
         game.time.events.add(Phaser.Timer.SECOND * 3, function () {
-            //game.state.start('menu');
+            game.state.start('menu');
         }, this);
 
     },
@@ -146,7 +204,7 @@ window.onload = function() {
     game.state.add('boot', bootState);
     game.state.add('load', loadState);
     game.state.add('splash', splashState);
-    //game.state.add('menu', menuState);
+    game.state.add('menu', menuState);
     //game.state.add('game', gameState);
 
     game.state.start('boot');
