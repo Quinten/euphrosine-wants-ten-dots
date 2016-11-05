2da9fd70d4abdf4aa17954abf5f975c0ac5186b7
Quinten Clause <quinten.clause@studioemma.eu>
2016-11-05 21:51:44 +0100

    Add gravity and collisions with tilemap

--- src/game/states/game.js
+++ src/game/states/game.js
@@ -13,8 +13,13 @@ var gameState = {
 
     create: function () {
 
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.physics.arcade.gravity.y = 500;
+
         this.map = game.add.tilemap('level');
         this.map.addTilesetImage('tiles');
+
+        this.map.setCollisionByExclusion([]);
 
         this.layer = this.map.createLayer('platformlayer');
         //this.layer.debug = true;
@@ -22,12 +27,23 @@ var gameState = {
 
         this.player = game.add.sprite(32, 32, 'player');
 
+        game.physics.enable(this.player, Phaser.Physics.ARCADE);
+        this.player.body.bounce.y = 0.2;
+        this.player.body.collideWorldBounds = true;
+        this.player.body.setSize(48, 48, 0, 0); // will need tweaking when we have a graphic
+
         this.player.animations.add('run-left', [0], 12, true);
         this.player.animations.add('run-right', [0], 12, true);
         this.player.animations.add('jump-left', [0], 12, true);
         this.player.animations.add('jump-right', [0], 12, true);
 
         game.camera.follow(this.player);
+
+    },
+
+    update: function () {
+
+        game.physics.arcade.collide(this.player, this.layer);
 
     },
 
