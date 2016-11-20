fb782896d2098c0e456f02a789df49210155f8f4
Quinten Clause <quinten.clause@studioemma.eu>
2016-11-20 11:59:29 +0100

    Add stageMask so clouds dont overflow on 2048+

--- src/game/main.js
+++ src/game/main.js
@@ -24,6 +24,8 @@ WebFontConfig = {
     }
 };
 
+var stageMask;
+
 window.onload = function() {
 
     game = new Phaser.Game("100%", "100%", Phaser.CANVAS, '');
--- src/game/states/boot.js
+++ src/game/states/boot.js
@@ -7,6 +7,11 @@ var bootState = {
         game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
         game.scale.fullScreenScaleMode = Phaser.ScaleManager.RESIZE;
 
+        stageMask = new PIXI.Graphics();
+        stageMask.beginFill(0x00FF00);
+        stageMask.drawRect(0,0,2048,2048);
+        game.stage.mask = stageMask;
+
         game.stage.smoothed = false; // none pixelated effect
         game.input.mouse.capture = true;
 
