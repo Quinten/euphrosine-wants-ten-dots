7a19435a4376a538674f398b5d548511f6b09d79
Quinten Clause <quinten.clause@studioemma.eu>
2016-11-22 21:04:36 +0100

    Adjust sizes/content of text for mobile

--- code/game.js
+++ code/game.js
@@ -334,7 +334,8 @@ var gameState = {
 
         game.onPause.add(this.onGamePause, this);
         game.onResume.add(this.onGameResume, this);
-        this.pausedText = this.createText(game.camera.width / 2, game.camera.height / 2, 'Click anywhere to resume game', colors.normalStroke, 42);
+        var downAction = (game.device.touch) ? 'Tap' : 'Click';
+        this.pausedText = this.createText(game.camera.width / 2, game.camera.height / 2, downAction + ' anywhere to resume game', colors.normalStroke, 42);
         this.pausedText.visible = false;
         this.dotBlinkTimer = game.time.now;
 
@@ -780,9 +781,17 @@ var menuState = {
         this.menuGroup.x = game.world.centerX;
         this.menuGroup.y = game.world.centerY;
 
-        var textsprite = this.menuGroup.add(this.createText(0, -150, 'Euphrosine wants ten dots', colors.normalStroke, 69));
-        var instructionsSprite = this.menuGroup.add(this.createText(0, 0, 'Catch dice to collect 10 dots\nUse left + right arrow keys to run + spacebar to jump\nUp arrow in the air to float\nUp + then other arrow keys in green area to climb', colors.normalStroke, 28));
-        this.textstart = this.menuGroup.add(this.createText(0, 150, 'Hit spacebar to start', colors.normalStroke, 42));
+        var textsprite;
+        var instructionsSprite;
+        if (game.device.touch) {
+            textsprite = this.menuGroup.add(this.createText(0, -120, 'Euphrosine wants ten dots', colors.normalStroke, 36));
+            instructionsSprite = this.menuGroup.add(this.createText(0, 0, 'Catch dice to collect 10 dots\nHold and drag one finger to run,\nDrag up to climb and float.\nWhile holding tap with a second finger to jump.', colors.normalStroke, 20));
+            this.textstart = this.menuGroup.add(this.createText(0, 120, 'Tap anywhere to start', colors.normalStroke, 32));
+        } else {
+            textsprite = this.menuGroup.add(this.createText(0, -150, 'Euphrosine wants ten dots', colors.normalStroke, 69));
+            instructionsSprite = this.menuGroup.add(this.createText(0, 0, 'Catch dice to collect 10 dots\nUse left + right arrow keys to run + spacebar to jump\nUp arrow in the air to float\nUp + then other arrow keys in green area to climb', colors.normalStroke, 28));
+            this.textstart = this.menuGroup.add(this.createText(0, 150, 'Hit spacebar to start', colors.normalStroke, 42));
+        }
 
         //var startimage = this.menuGroup.add(game.add.sprite(0, 0, 'startscreen'));
         //startimage.anchor.setTo(0.5);
@@ -795,7 +804,7 @@ var menuState = {
 
         this.switched = false;
 
-        if (!game.device.desktop) {
+        if (game.device.touch) {
             game.input.onDown.add(this.startFullScreen, this);
         }
 
@@ -881,7 +890,7 @@ var splashState = {
         text.anchor.setTo(0.5);
 
         text.font = fontName;
-        text.fontSize = 42;
+        text.fontSize = (game.device.touch) ? 32 : 42;
 
         text.fill = colors.normalStroke;
 
