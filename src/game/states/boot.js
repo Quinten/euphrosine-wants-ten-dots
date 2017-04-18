1d21646ff8ca7559f109e207bd6a1f6ea8fccad8
Quinten Clause <quinten.clause@studioemma.eu>
2017-04-18 22:25:24 +0200

    Add fullscreen button

--- src/game/states/boot.js
+++ src/game/states/boot.js
@@ -18,6 +18,30 @@ var bootState = {
         // at first do not pause game when browser window loses focus
         game.stage.disableVisibilityChange = true;
 
+        // show fullscreen button? only on desktop and not on kongregate :(
+        if (game.device.desktop && document.referrer.indexOf('kongregate') <= -1) {
+
+            document.getElementById('fs-button').style.display = 'block';
+
+            document.getElementById('fs-button').addEventListener('click', function (e) {
+                if (game.scale.isFullScreen) {
+                    game.scale.stopFullScreen();
+                } else {
+                    game.scale.startFullScreen(false);
+                }
+                document.getElementById('fs-button').style.display = 'none';
+            });
+
+            game.scale.onFullScreenChange.add(function (scale) {
+                if (scale.isFullScreen) {
+                    document.getElementById('fs-button').style.display = 'none';
+                } else {
+                    document.getElementById('fs-button').style.display = 'block';
+                }
+            }, this);
+
+        }
+
         game.input.gamepad.start();
         pad1 = game.input.gamepad.pad1;
 
